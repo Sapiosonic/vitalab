896e6dd19a1b18682f3f683d26538a82b1d7af98
Radamés Rodrigues <radames.idiomas@gmail.com>
2023-10-08 22:26:18 -0300

    "Criando o gerenciamento de exames"

--- exames/models.py
+++ exames/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe
+
 
 class TiposExames(models.Model):
   tipo_choices = (
@@ -16,6 +18,7 @@ class TiposExames(models.Model):
   def __str__(self):
     return self.nome
 
+
 class SolicitacaoExame(models.Model):
   choice_status = (
       ('E', 'Em análise'),
@@ -30,7 +33,17 @@ class SolicitacaoExame(models.Model):
 
   def __str__(self):
     return f'{self.usuario} | {self.exame.nome}'
-    
+  
+  def badge_template(self):
+    if self.status == 'E':
+      classes_css = 'bg-warning text-dark'
+      texto = 'Em análise'
+    elif self.status == 'F':
+      classes_css = 'bg-success'
+      texto = 'Finalizado'
+    return mark_safe(f"<span class='badge bg-primary {classes_css}'>{texto}</span>")
+  
+
 class PedidosExames(models.Model):
   usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
   exames = models.ManyToManyField(SolicitacaoExame)
