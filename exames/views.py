ad146b2e1b2aaaba11bb43383f49331ac14b5f51
Radamés <radames.idiomas@gmail.com>
2024-05-29 00:07:04 -0300

    feat (solicitar_exame): Implementa formulários dinâmicos

--- exames/views.py
+++ exames/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import TiposExames
+
 
 @login_required
 def solicitar_exames(request):
+    tipos_exames = TiposExames.objects.all()
     if request.method == "GET":
-        return render(request, 'solicitar_exames.html')
+        return render(request, 'solicitar_exames.html', {'tipos_exames': tipos_exames})
+    else:
+        exames_id = request.POST.getlist('exames')
+
+        solicitacao_exames = TiposExames.objects.filter(id__in=exames_id)
+        #preco_total = solicitacao_exames.aggregate(total=Sum('preco'))['total']
+        preco_total = 0
+        for i in solicitacao_exames:
+            preco_total += i.preco
+        
+        return render(request, 'solicitar_exames.html', 
+            {
+                'solicitacao_exames': solicitacao_exames, 
+                'preco_total': preco_total, 
+                'tipos_exames': tipos_exames
+            }
+        )
